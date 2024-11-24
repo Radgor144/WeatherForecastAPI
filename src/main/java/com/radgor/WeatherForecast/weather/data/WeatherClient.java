e373a021c77b7a16707789afcf2fbb415d222d1a
Radek <radek.goralik@interia.pl>
2024-11-24 20:28:36 +0100

    added new Dockerfile23

--- src/main/java/com/radgor/WeatherForecast/weather/data/WeatherClient.java
+++ src/main/java/com/radgor/WeatherForecast/weather/data/WeatherClient.java
@@ -1,11 +1,12 @@
 package com.radgor.WeatherForecast.weather.data;
 
+import com.radgor.WeatherForecast.weather.config.RetryerConfig;
 import com.radgor.WeatherForecast.weather.config.WeatherClientConfiguration;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
-@FeignClient(name = "weather-client", configuration = {WeatherClientConfiguration.class})
+@FeignClient(name = "weather-client", configuration = {WeatherClientConfiguration.class, RetryerConfig.class})
 public interface WeatherClient {
 
     @GetMapping("/v1/forecast")
