97f88809ff7f4ec356d95261ee4073474a031871
Radek <radek.goralik@interia.pl>
2024-11-24 15:52:38 +0100

    added validation of variables in controllers

--- src/main/java/com/radgor/WeatherForecast/weather/ForecastController.java
+++ src/main/java/com/radgor/WeatherForecast/weather/ForecastController.java
@@ -2,6 +2,11 @@ package com.radgor.WeatherForecast.weather;
 
 import com.radgor.WeatherForecast.weather.data.daily.ProcessedWeatherDailyData;
 import com.radgor.WeatherForecast.weather.data.summary.SummaryWeatherData;
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.validation.annotation.Validated;
@@ -20,12 +25,16 @@ public class ForecastController {
     private final WeatherService weatherService;
 
     @GetMapping("/forecast")
-    public ProcessedWeatherDailyData getWeatherData(@RequestParam double latitude, @RequestParam double longitude) {
+    public ProcessedWeatherDailyData getWeatherData(
+            @RequestParam @Valid @Min(-90) @Max(90) double latitude,
+            @RequestParam @Valid @Min(-180) @Max(180) double longitude) {
         return weatherService.getProcessedWeatherData(latitude, longitude);
     }
 
     @GetMapping("/summary")
-    public SummaryWeatherData getSummaryWeatherData(@RequestParam double latitude, @RequestParam double longitude) {
+    public SummaryWeatherData getSummaryWeatherData(
+            @RequestParam @Valid @Min(-90) @Max(90) double latitude,
+            @RequestParam @Valid @Min(-180) @Max(180) double longitude) {
         return weatherService.getSummaryWeatherData(latitude, longitude);
     }
 
