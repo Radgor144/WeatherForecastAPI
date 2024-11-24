886b585c7bebff5b70305131e316e683023d424e
Radek <radek.goralik@interia.pl>
2024-11-24 16:31:41 +0100

    added retryer to WeatherClient

---
+++ src/main/java/com/radgor/WeatherForecast/weather/config/RetryerConfig.java
@@ -0,0 +1,13 @@
+package com.radgor.WeatherForecast.weather.config;
+
+import feign.Retryer;
+import org.springframework.context.annotation.Bean;
+
+import java.util.concurrent.TimeUnit;
+
+public class RetryerConfig{
+    @Bean
+    public Retryer retryer(){
+        return new Retryer.Default(100L, TimeUnit.SECONDS.toMillis(3L), 5);
+    }
+}
